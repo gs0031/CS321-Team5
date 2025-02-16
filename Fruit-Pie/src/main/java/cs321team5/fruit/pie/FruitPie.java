6aa719d3ed36e5b5858e5d4e64a754439cd369e5
Garett Sutton <gs0031@uah.edu>
2025-02-16 16:31:31 -0600

    empty netbeans project

---
+++ Fruit-Pie/src/main/java/cs321team5/fruit/pie/FruitPie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cs321team5.fruit.pie;
+
+/**
+ *
+ * @author sutto
+ */
+public class FruitPie {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
