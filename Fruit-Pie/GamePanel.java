49606bb06dcf133d1e6bb8c09a99cf5ed68fb6d6
Zumorinzu <brianewilliams256@gmail.com>
2025-02-26 22:17:08 -0600

    Update GamePanel.java
    There has been a limiter to the FPS, new variables for the fruit's movement, and a game clock has been added to allow updates to be made to the game in real time.

--- Fruit-Pie/GamePanel.java
+++ Fruit-Pie/GamePanel.java
@@ -6,6 +6,11 @@ package Test;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.JPanel;
 
 /**
@@ -23,14 +28,24 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 760 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
     
+    //FPS
+    int FPS = 60;
+    
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread; // Keeps the program runing until you stop it
     
+    //Set fruit's default position
+    int fruitx = 450;
+    int fruity = 100;
+    int fruitSpeed = 4;
     
     public GamePanel()
     {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.yellow);
+        this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
                 
     }
     
@@ -44,9 +59,65 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() 
     {
+        double drawInterval = 1000000000/FPS; // draws the screen 60 times per second
+        double nextDrawTime = System.nanoTime() + drawInterval;
+            
         while(gameThread != null) 
         {
-            System.out.println("The game loop is running");
+            
+            // 1. UPDATE: Update information such as fruit positions
+            update();
+            
+            // 2. DRAW: draw the screen with the updated information
+            repaint();
+            
+            
+            try 
+            {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+                
+                if(remainingTime < 0)
+                {
+                    remainingTime = 0;
+                }
+                
+                Thread.sleep((long) remainingTime);
+                
+                nextDrawTime += drawInterval;
+            } 
+            
+            catch (InterruptedException ex) 
+            {
+                Logger.getLogger(GamePanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+    }
+    
+    public void update()
+    {
+        if(keyH.leftPressed == true)
+        {
+            fruitx -= fruitSpeed;
+        }
+        
+        else if(keyH.rightPressed == true)
+        {
+            fruitx += fruitSpeed;
+        }
+    }
+    
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        
+        Graphics2D g2 = (Graphics2D)g;
+        
+        g2.setColor(Color.white);
+        
+        g2.fillRect(fruitx, fruity, tileSize, tileSize);
+        
+        g2.dispose();
     }
 }
