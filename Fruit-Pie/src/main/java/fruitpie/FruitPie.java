cfded55928dd8729caa7e1533ea6feaa61dcb25e
drac555 <36717420+drac555@users.noreply.github.com>
2025-02-26 19:50:37 -0600

    Code refactor, including new changes into main project
    added brians stuff to the main packages -- also added packages for the main program
    
    main class is Fruitpie.java

---
+++ Fruit-Pie/src/main/java/fruitpie/FruitPie.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fruitpie;
+
+import graphics.GamePanel;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author brian
+ */
+public class FruitPie 
+{
+    public static void main(String[] args) 
+    {
+      JFrame window = new JFrame();  
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      window.setResizable(true);
+      window.setTitle("Supa-window");
+      
+      GamePanel gamePanel = new GamePanel();
+      window.add(gamePanel);
+      
+      window.pack();
+      
+      window.setLocationRelativeTo(null);
+      window.setVisible(true);
+        
+      
+    }
+    
+}
