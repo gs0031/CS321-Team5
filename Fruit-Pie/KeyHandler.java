e67adc4a57bb47f8487d0c8c1d6b886ffb9756fa
Zumorinzu <brianewilliams256@gmail.com>
2025-04-07 15:46:15 -0500

    Update KeyHandler.java
    Updated to allow for our current inputs("a" and "d" to move left and right, and mouse press to drop

--- Fruit-Pie/KeyHandler.java
+++ Fruit-Pie/KeyHandler.java
@@ -1,58 +1,105 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Test;
+package fruitpie;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-/**
- *
- * @author brian
- */
-public class KeyHandler implements KeyListener {
+public class InputHandler implements EventHandler<KeyEvent>, MouseMotionListener  
+{
+
+    public boolean leftPressed, rightPressed, dropPressed, escPressed, validMouse;
     
-    public boolean leftPressed, rightPressed;
-
+    public float xMouse;
+    
+    
+    
     @Override
-    public void keyTyped(KeyEvent e) 
+    public void mouseMoved(MouseEvent e) 
     {
-        // Not Needed ----------------------------------------
+        float code = e.getXOnScreen();
+        validMouse = true;
+        xMouse = code;
+        System.out.println("5005");
     }
 
-    @Override
-    public void keyPressed(KeyEvent e) 
-    {
-        int code = e.getKeyCode();
-        
-        if (code == KeyEvent.VK_A)
+    public void mouseReleased(MouseEvent e) 
         {
-            leftPressed = true;
+            float code = e.getButton();
+
+            if(code == MouseEvent.BUTTON1)
+            {
+                dropPressed = false;
+            }
         }
-        
-        if (code == KeyEvent.VK_D)
-        {
-            rightPressed = true;
-        }
-        
-    }
+
 
     @Override
-    public void keyReleased(KeyEvent e) 
+    public void mouseDragged(MouseEvent e) 
+    {}
+    
+    
+    
+    @Override
+    public void handle(KeyEvent e) 
     {
-        int code = e.getKeyCode();
-        
-        if (code == KeyEvent.VK_A)
+        if (e.getEventType() == KeyEvent.KEY_PRESSED) 
         {
-            leftPressed = false;
+            KeyCode code = e.getCode();
+
+            if (code == KeyCode.A) 
+            {
+                leftPressed = true;
+            }
+
+            if (code == KeyCode.D) 
+            {
+                rightPressed = true;
+            }
+
+            if (code == KeyCode.M) 
+            {
+                dropPressed = true;
+            }
+
+            // Handle the Esc key
+            if (code == KeyCode.ESCAPE) 
+            {
+                escPressed = true;
+            }
         }
-        
-        if (code == KeyEvent.VK_D)
+
+        if (e.getEventType() == KeyEvent.KEY_RELEASED) 
         {
-            rightPressed = false;
+            KeyCode code = e.getCode();
+
+            if (code == KeyCode.A) 
+            {
+                leftPressed = false;
+            }
+
+            if (code == KeyCode.D) 
+            {
+                rightPressed = false;
+            }
+
+            if (code == KeyCode.M) 
+            {
+                dropPressed = false;
+            }
+
+            // Handle the Esc key
+            if (code == KeyCode.ESCAPE) 
+            {
+                escPressed = false;
+            }
+
         }
-        
+    }
+    
+}
+
     }
     
 }
